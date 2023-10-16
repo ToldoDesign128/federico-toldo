4ee4d40e8ca2b564e5a2a9d9cb5dbb1409f47403
Federico <fede.toldo96@gmail.com>
2023-10-16 20:55:00 +0200

    fix mobile header

--- header.php
+++ header.php
@@ -17,44 +17,44 @@
       <div class="relative bg-gray-200 dark:bg-gray-800 z-40">
          <div class="flex container mx-auto py-5 px-4 justify-between items-center">
             <div class="lg:w-40 w-auto flex items-center">
-               <button id="hamburgerButton" class="group py-4 px-[.5rem] mr-2 bg-transparent hover:bg-gray-900 dark:hover:bg-gray-100 border border-gray-900 dark:border-gray-100 rounded-full">
-                  <span class="flex h-px w-8 bg-gray-900 group-hover:bg-gray-100 dark:bg-gray-100 dark:group-hover:bg-gray-900"></span>
-                  <span class="flex h-px w-8 mt-3 bg-gray-950 group-hover:bg-gray-100 dark:bg-gray-100 dark:group-hover:bg-gray-900"></span>
+               <button id="hamburgerButton" class="group py-4 px-[.5rem] mr-2 bg-transparent lg:hover:bg-gray-900 dark:lg:hover:bg-gray-100 border border-gray-900 dark:border-gray-100 rounded-full">
+                  <span class="flex h-px w-8 bg-gray-900 group-lg:hover:bg-gray-100 dark:bg-gray-100 dark:group-lg:hover:bg-gray-900"></span>
+                  <span class="flex h-px w-8 mt-3 bg-gray-950 group-lg:hover:bg-gray-100 dark:bg-gray-100 dark:group-lg:hover:bg-gray-900"></span>
                </button>
                <p id="menuOpen" class="lg:flex hidden text-gray-900 dark:text-gray-100">Menu</p>
                <p id="menuClose" class="hidden text-gray-900 dark:text-gray-100">Close</p>
             </div>
-            <a href="<?php echo home_url(); ?>" class="w-auto font-semibold text-gray-900 dark:text-gray-100 hover:text-gray-600 dark:hover:text-gray-300 hover:tracking-wide transition-all duration-300">
+            <a href="<?php echo home_url(); ?>" class="w-auto font-semibold text-gray-900 dark:text-gray-100 lg:hover:text-gray-600 dark:lg:hover:text-gray-300 lg:hover:tracking-wide transition-all duration-300">
                Federico Toldo
             </a>
-            <a id="headerCta" href="<?php echo get_page_link(get_page_by_title('contatti')->ID); ?>" class="lg:flex hidden w-auto text-gray-900 hover:text-gray-100 dark:text-gray-100 dark:hover:text-gray-900 border bg-transparent hover:bg-gray-900 dark:hover:bg-gray-100 border-gray-900 dark:border-gray-100 rounded py-2 px-5">
+            <a id="headerCta" href="<?php echo get_page_link(get_page_by_title('contatti')->ID); ?>" class="lg:flex hidden w-auto text-gray-900 lg:hover:text-gray-100 dark:text-gray-100 dark:lg:hover:text-gray-900 border bg-transparent lg:hover:bg-gray-900 dark:lg:hover:bg-gray-100 border-gray-900 dark:border-gray-100 rounded py-2 px-5">
                Contattami
             </a>
          </div>
       </div>
       <span id="headerMenuBg" class="flex h-px w-full bg-gray-300 dark:bg-gray-700 z-50"></span>
-      <div id="menu" class="relative bg-gray-200 dark:bg-gray-800 py-16 px-4 z-30">
+      <div id="menu" class="relative bg-gray-200 dark:bg-gray-800 md:py-16 py-8 px-4 z-30">
          <div class="flex md:flex-row flex-wrap container w-full max-h-fit mx-auto justify-between">
             <!-- Section info -->
-            <div class="lg:w-4/12 w-full lg:my-0 my-12 lg:order-1 order-3">
-               <div class="flex justify-between mb-12">
+            <div class="lg:w-4/12 w-full lg:my-0 md:my-12 my-6 lg:order-1 order-3">
+               <div class="md:flex hidden justify-between mb-12">
                   <ul class="w-1/2">
                      <li class="my-2 text-gray-900 dark:text-gray-100">federicotoldo.com</li>
                      <li class="my-2 text-gray-900 dark:text-gray-100">P.IVA 123456789</li>
                      <li class="my-2 text-gray-900 dark:text-gray-100">©2023 Federico Toldo</li>
                   </ul>
                   <ul class="lg:w-1/2 w-5/12">
-                     <li class="my-2"><a class="flex text-gray-900 dark:text-gray-100 hover:text-gray-600 dark:hover:text-gray-300 hover:tracking-wide transition-all duration-300" href="mailto:info@federicotoldo.com" target="_blank">
+                     <li class="my-2"><a class="flex text-gray-900 dark:text-gray-100 lg:hover:text-gray-600 dark:lg:hover:text-gray-300 lg:hover:tracking-wide transition-all duration-300" href="mailto:info@federicotoldo.com" target="_blank">
                            <img class="mr-2 dark:hidden" src="<?php echo get_template_directory_uri() . '/img/icon/mdi_email.svg'; ?>" alt="">
                            <img class="mr-2 hidden dark:flex" src="<?php echo get_template_directory_uri() . '/img/icon/mdi_email_w.svg'; ?>" alt="">
                            info@federicotoldo.com</a>
                      </li>
-                     <li class="my-2"><a class="flex text-gray-900 dark:text-gray-100 hover:text-gray-600 dark:hover:text-gray-300 hover:tracking-wide transition-all duration-300" href="#" target="_blank">
+                     <li class="my-2"><a class="flex text-gray-900 dark:text-gray-100 lg:hover:text-gray-600 dark:lg:hover:text-gray-300 lg:hover:tracking-wide transition-all duration-300" href="#" target="_blank">
                            <img class="mr-2 dark:hidden" src="<?php echo get_template_directory_uri() . '/img/icon/mdi_linkedin.svg'; ?>" alt="">
                            <img class="mr-2 hidden dark:flex" src="<?php echo get_template_directory_uri() . '/img/icon/mdi_linkedin_w.svg'; ?>" alt="">
                            Linkedin</a>
                      </li>
-                     <li class="my-2"><a class="flex text-gray-900 dark:text-gray-100 hover:text-gray-600 dark:hover:text-gray-300 hover:tracking-wide transition-all duration-300" href="#" target="_blank">
+                     <li class="my-2"><a class="flex text-gray-900 dark:text-gray-100 lg:hover:text-gray-600 dark:lg:hover:text-gray-300 lg:hover:tracking-wide transition-all duration-300" href="#" target="_blank">
                            <img class="mr-2 dark:hidden" src="<?php echo get_template_directory_uri() . '/img/icon/mdi_github.svg'; ?>" alt="">
                            <img class="mr-2 hidden dark:flex" src="<?php echo get_template_directory_uri() . '/img/icon/mdi_github_w.svg'; ?>" alt="">
                            GitHub</a>
@@ -62,7 +62,7 @@
                   </ul>
                </div>
                <div class="w-full flex justify-between items-center">
-                  <a href="#" class="text-gray-900 dark:text-gray-100 hover:text-gray-600 dark:hover:text-gray-300 hover:tracking-wide transition-all duration-300">Privacy Policy</a>
+                  <a href="#" class="text-gray-900 dark:text-gray-100 lg:hover:text-gray-600 dark:lg:hover:text-gray-300 lg:hover:tracking-wide transition-all duration-300">Privacy Policy</a>
                   <!-- Icona dark mode -->
                   <div class="hidden dark:flex">
                      <p class="text-gray-100 mr-2">Go light</p>
@@ -88,7 +88,7 @@
                </nav>
             </div>
             <!-- Section stiky post -->
-            <div class="lg:w-3/12 md:w-5/12 w-full lg:order-3 order-2">
+            <div class="lg:w-3/12 md:w-5/12 md:block hidden w-full lg:order-3 order-2">
                <?php
                $loop = new WP_Query(array(
                   'post_type'         => 'progetti',
@@ -103,7 +103,7 @@
 
                   <a href="<?php echo the_permalink(); ?>">
                      <article class="w-full h-full relative rounded">
-                        <?php echo get_the_post_thumbnail(null, 'large', ['class' => 'w-full h-full brightness-50 hover:brightness-75 rounded transition-all duration-300']); ?>
+                        <?php echo get_the_post_thumbnail(null, 'large', ['class' => 'w-full h-full brightness-50 lg:hover:brightness-75 rounded transition-all duration-300']); ?>
                         <div class="absolute bottom-0 p-4 text-gray-100 text-4xl z-10">
                            <p class="text-gray-100 text-4xl font-medium">Dai un occhiata al mio ultimo progetto</p>
                            <p class="text-gray-300 text-xl">www.weatherwatersand.com</p> <!-- Da sostituire con il field del URL -->
